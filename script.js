6d5f7e0dffca2fa8e2df1828911d1bfc9b07cfdf
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-12 22:41:39 -0300

    update final

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// selecting elements
+// Selecting elements
 const player0El = document.querySelector('.player--0');
 const player1El = document.querySelector('.player--1');
-const score0El = document.getElementById('score--0');
+const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
@@ -11,68 +11,85 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// Starting condition
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores, currentScore, activePlayer, playing;
 
-const score = [0, 0];
-let currentScore = 0;
-let activePLayer = 0;
-let playing = true;
-
-const switchplayer = function () {
-  document.getElementById(`current--${activePLayer}`).textContent = 0;
+// Starting conditions
+const init = function () {
+  scores = [0, 0];
   currentScore = 0;
-  activePLayer = activePLayer === 0 ? 1 : 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+init();
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
 
-// rolling dice functionality
+// Rolling dice functionality
 btnRoll.addEventListener('click', function () {
   if (playing) {
-    // 1. generating a random dice roll
+    // 1. Generating a random dice roll
     const dice = Math.trunc(Math.random() * 6) + 1;
-    // 2. display dice
+
+    // 2. Display dice
     diceEl.classList.remove('hidden');
-    // console.log(dice);
     diceEl.src = `dice-${dice}.png`;
 
-    // check for rolled 1
+    // 3. Check for rolled 1
     if (dice !== 1) {
-      // add dice to current score
+      // Add dice to current score
       currentScore += dice;
-      document.getElementById(`current--${activePLayer}`).textContent =
+      document.getElementById(`current--${activePlayer}`).textContent =
         currentScore;
     } else {
-      //Switch to nest player
-      switchplayer();
+      // Switch to next player
+      switchPlayer();
     }
   }
 });
 
 btnHold.addEventListener('click', function () {
   if (playing) {
-    // 1. add current score to active player's score
-    score[activePLayer] += currentScore;
-    document.getElementById(`score--${activePLayer}`).textContent =
-      score[activePLayer];
-    // 2. check if o player's score is >=100
-    if (score[activePLayer] >= 20) {
-      // finish the game
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore
+
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
 
       document
-        .querySelector(`.player--${activePLayer}`)
+        .querySelector(`.player--${activePlayer}`)
         .classList.add('player--winner');
       document
-        .querySelector(`.player--${activePLayer}`)
+        .querySelector(`.player--${activePlayer}`)
         .classList.remove('player--active');
     } else {
-      // switch to the next player
-      switchplayer();
+      // Switch to the next player
+      switchPlayer();
     }
   }
 });
+
+btnNew.addEventListener('click', init);
