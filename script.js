e44174ff1e018ef9bd3b37d433ebe5e0ce68277f
TharlyRodrigues <tharly.portifolio@gmail.com>
2024-01-12 22:01:49 -0300

    improving the gamer's finishing logic

--- script.js
+++ script.js
@@ -18,45 +18,59 @@ diceEl.classList.add('hidden');
 
 const score = [0, 0];
 let currentScore = 0;
-let activePLay = 0;
+let activePLayer = 0;
+let playing = true;
 
 const switchplayer = function () {
-  document.getElementById(`current--${activePLay}`).textContent = 0;
+  document.getElementById(`current--${activePLayer}`).textContent = 0;
   currentScore = 0;
-  activePLay = activePLay === 0 ? 1 : 0;
+  activePLayer = activePLayer === 0 ? 1 : 0;
   player0El.classList.toggle('player--active');
   player1El.classList.toggle('player--active');
 };
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1. generating a random dice roll
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  // 2. display dice
-  diceEl.classList.remove('hidden');
-  // console.log(dice);
-  diceEl.src = `dice-${dice}.png`;
+  if (playing) {
+    // 1. generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. display dice
+    diceEl.classList.remove('hidden');
+    // console.log(dice);
+    diceEl.src = `dice-${dice}.png`;
 
-  // check for rolled 1
-  if (dice !== 1) {
-    // add dice to current score
-    currentScore += dice;
-    document.getElementById(`current--${activePLay}`).textContent =
-      currentScore;
-  } else {
-    //Switch to nest player
-    switchplayer();
+    // check for rolled 1
+    if (dice !== 1) {
+      // add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePLayer}`).textContent =
+        currentScore;
+    } else {
+      //Switch to nest player
+      switchplayer();
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  // 1. add current score to active player's score
-  score[activePLay] += currentScore;
-  document.getElementById(`score--${activePLay}`).textContent =
-    score[activePLay];
-  // 2. check if o player's score is >=100
-  // finish the game
-
-  // switch to the next player
-  switchplayer();
+  if (playing) {
+    // 1. add current score to active player's score
+    score[activePLayer] += currentScore;
+    document.getElementById(`score--${activePLayer}`).textContent =
+      score[activePLayer];
+    // 2. check if o player's score is >=100
+    if (score[activePLayer] >= 100) {
+      // finish the game
+      playing = false;
+      document
+        .querySelector(`.player--${activePLayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePLayer}`)
+        .classList.remove('player--active');
+    } else {
+      // switch to the next player
+      switchplayer();
+    }
+  }
 });
